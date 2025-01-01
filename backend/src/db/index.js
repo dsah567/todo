0116d2c5383df777d8f900c5e124a245067a9787
Dipu Sah <sahd7929@gmail.com>
2025-01-01 20:50:46 +0530

    backend connection verified

---
+++ backend/src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB= async()=>{
+    try{
+        const connectionInstances = await mongoose.connect(`${process.env.MONGODB_URI}${process.env.DB_NAME}`)
+        console.log(`\n MongoDB connected DB HOST ${connectionInstances.connection.host}`);
+    }
+    catch(err){
+        console.log("MONGODB connection FAILED ", err);
+        process.exit(1)
+    }
+}
+
+export default connectDB
---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv"
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: './.env'
+})
+
+const port = process.env.PORT || 8080
+connectDB()
+.then(()=>{
+    app.on("errror", (error) => {
+        console.log("ERRR: ", error);
+        throw error
+    })
+    app.listen(port,()=>{
+        console.log(`Server is listening at port ${port}`)})
+})
+.catch((err)=>{
+    console.log("MongoDb Connection failed :", err);
+});
